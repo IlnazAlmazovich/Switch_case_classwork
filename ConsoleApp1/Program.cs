4feaad2007a4b958dcb483ec00eacefcdcab5a0c
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2023-10-01 10:50:53 +0300

    ffff

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            string type;//определяем тип числа
+            switch(a)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 9:
+                    type = "Число является нечётным";
+                    break;
+
+                case 2:
+                case 4:
+                case 6:
+                case 8:
+                case 10:
+                    type = "Число является чётным";
+                break;
+
+                default:
+                    type = "Вы ввели число 0 или число больше 10!";
+                    break;
+            }
+            Console.WriteLine(type);
+        }
+    }
+}
---
+++ Switch_case/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //По введённому пользователем числу определить название этого числа. (Взять числа от 1 до 5).
+
+            Console.WriteLine("Введите число: ");
+            int number = int.Parse(Console.ReadLine()); // переменная для запоминания числа
+            string name; //переменная для записи названия числа
+            
+            switch(number)
+            {
+                case 1:  //если ввели число 1
+                    name = "Единица";
+                break;
+
+                case 2:  //если ввели число 2
+                    name = "Двойка";
+                    break;
+                case 3:  //если ввели число 3
+                    name = "Тройка";
+                    break;
+                case 4: //если ввели число 4
+                    name = "Четвёрка";
+                    break;
+                case 5:  //если ввели число 5
+                    name = "Пятёрка";
+                    break;
+                default: //ввели данные больше чем в заданном диапазоне
+                    name = "Таких чисел мы не знаем";
+                    break;
+            }
+            Console.WriteLine(name); //вывод данных в консоль
+
+        }
+    }
+}
